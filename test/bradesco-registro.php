22bbd3b140101eb864fec1e737e9edb0b7c5f941
rlzdesenv <86244364+rlzdesenv@users.noreply.github.com>
2024-11-28 14:27:00 -0300

    Update bradesco-registro.php

--- test/bradesco-registro.php
+++ test/bradesco-registro.php
@@ -2,41 +2,49 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 
-use Boleto\Entity\Pagador;
+use Boleto\Bank\BradescoService;
 use Boleto\Entity\Beneficiario;
+use Boleto\Entity\Certificado;
+use Boleto\Entity\Desconto;
 use Boleto\Entity\Juros;
 use Boleto\Entity\Multa;
-use \Boleto\Entity\Certificado;
+use Boleto\Entity\Pagador;
 
 try {
 
-    $certificado = new Certificado('xxx.pfx', 'xxxx');
-    $pagador = new Pagador('Fulano da Silva', '43563609829', 'Rua Antenor Guirlanda', '15', null, 'Casa Verde', 'São Paulo', 'SP', '02514-010');
-    $beneficiario = new Beneficiario('Fulano da Silva', '13.954.351/0001-83', 'Rua Antenor Guirlanda', '15', null, 'Casa Verde', 'São Paulo', 'SP', '02514-010');
+    $certificado = new Certificado('certificado.homologacao.pfx', '123456');
+    $beneficiario = new Beneficiario('Fulano da Silva', '68542653101838', 'Rua Antenor Guirlanda', '15', null, 'Casa Verde', 'São Paulo', 'SP', '02514-010');
+    $pagador = new Pagador('Fulano da Silva', '62344900187', 'Rua Antenor Guirlanda', '15', null, 'Casa Verde', 'São Paulo', 'SP', '02514-010');
 
-    $vencimento = new DateTime('2018-03-21');
-	
-	
+    $vencimento = new DateTime('2024-11-30');
 
-    $juros = new Juros(Juros::Mensal, 2, new DateTime('2018-03-22'));
-    $multa = new Multa(2, new DateTime('2018-03-22'));
+    $juros = new Juros(Juros::Mensal, 2, new DateTime('2024-11-30'));
+    $multa = new Multa(2, new DateTime('2024-11-30'));
 
-    $bradesco = new \Boleto\Bank\BradescoService();
-    $bradesco->setEmissao($vencimento)
+    $desconto1 = new Desconto(1, 3, new DateTime('2024-11-27'));
+    $desconto2 = new Desconto(1, 2, new DateTime('2024-11-28'));
+    $desconto3 = new Desconto(1, 1, new DateTime('2024-11-29'));
+
+    $bradesco = new BradescoService();
+    $bradesco->setEmissao((new DateTime()))
         ->setVencimento($vencimento)
         ->setValor(100)
-        ->setNossoNumero(1000008)
-        ->setCarteira(26)
-        ->setAgencia('4852')
-        ->setConta('604')
+        ->setNossoNumero(80000000023)
+        ->setAgencia('3861')
+        ->setConta('41000')
         ->setPagador($pagador)
         ->setBeneficiario($beneficiario)
         ->setCertificado($certificado)
         ->setJuros($juros)
         ->setMulta($multa)
+        ->setDesconto($desconto1)
+        ->setDesconto($desconto2)
+        ->setDesconto($desconto3)
+        ->setSandbox(true)
+        ->setClientId('c79a7632-3549-4580-93c1-46a678b45103')
         ->send();
 
-    echo $bradesco->getLinhaDigitavel();
+    echo $bradesco->getLinhaDigitavel() . PHP_EOL . $bradesco->getCodigoBarras();
 
 } catch (\Exception $e) {
     echo $e->getMessage();
